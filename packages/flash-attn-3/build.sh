0113623800f65fd24e53c36dadb1ac469fde0fe3
Jon Allen <joallen@nvidia.com>
2025-10-30 15:41:37 -0700

    Add cu130_torch29

--- packages/flash-attn-3/build.sh
+++ packages/flash-attn-3/build.sh
@@ -14,7 +14,10 @@
 # limitations under the License.
 
 # https://github.com/Dao-AILab/flash-attention?tab=readme-ov-file#installation-and-features
-export MAX_JOBS=${MAX_JOBS:-$(($(nproc) / 2))}
+export MAX_JOBS=${MAX_JOBS:-$(($(nproc) / 4))}
+
+# https://github.com/Dao-AILab/flash-attention/blob/main/hopper/setup.py
+export FLASH_ATTENTION_FORCE_BUILD=TRUE
 
 pip wheel \
 	-v \
