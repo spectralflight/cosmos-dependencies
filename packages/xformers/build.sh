b817ee1bc72efc6b7f7ca1e315c74f8f95ed9038
Jon Allen <joallen@nvidia.com>
2025-10-31 10:54:43 -0700

    Update.

---
+++ packages/xformers/build.sh
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# https://github.com/facebookresearch/xformers?tab=readme-ov-file#installing-xformers
+
+# https://github.com/facebookresearch/xformers/blob/main/setup.py
+XFORMERS_BUILD_TYPE="Release"
+
+pip wheel \
+	-v \
+	--no-deps \
+	--no-build-isolation \
+	--check-build-dependencies \
+	--wheel-dir="${OUTPUT_DIR}" \
+	"git+https://github.com/facebookresearch/xformers.git@v${PACKAGE_VERSION}#egg=xformers" \
+	"$@"
